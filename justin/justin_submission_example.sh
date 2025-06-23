5b4262dacca8899ae61baee1b88785578b258249
Alex Himmel <ahimmel@fnal.gov>
2025-06-23 16:04:44 -0500

    Add safety check, correct output patterns

--- justin/justin_submission_example.sh
+++ justin/justin_submission_example.sh
@@ -2,10 +2,17 @@
 
 USERURL='https://fndcadoor.fnal.gov:2880/dune/scratch/users'
 
+# Check that INPUT_TAR_DIR is set
+if [[ -z "${INPUT_TAR_DIR:-}" ]]; then
+    echo "ERROR: INPUT_TAR_DIR environment variable is not set. Maybe you need to export it?" >&2
+    exit 2
+fi
+
 justin simple-workflow --image fnal-wn-sl7:latest  \
                        --name larsmc-test-$(date +%Y%m%d%H%M) \
                        --monte-carlo 6 \
                        --jobscript lars_mc_test.jobscript \
-                       --env INPUT_TAR_DIR="$INPUT_TAR_DIR" \
+                       --env INPUT_TAR_DIR=$INPUT_TAR_DIR \
                        --scope usertests \
-                       --output-pattern "*.*:$USERURL/$USER"
+                       --output-pattern "larsmc_*.root:$USERURL/$USER/justin" \
+                       --output-pattern "otherfiles_*.tgz:$USERURL/$USER/justin" # Remove if you only care about the MC output
